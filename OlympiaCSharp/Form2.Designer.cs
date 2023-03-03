bb810f7d694b2aa5e51118008776f22591db2f91
Quang Minh Nguyen <smileos@outlook.com>
2023-03-03 16:25:51 +0700

    Update version 0.0.11 Testing the Guna UI2 Framwork

--- OlympiaCSharp/Form2.Designer.cs
+++ OlympiaCSharp/Form2.Designer.cs
@@ -28,25 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(236, 155);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(461, 136);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
@@ -56,6 +45,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
